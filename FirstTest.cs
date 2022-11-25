74105a1f8674159cb76a80e711d2dec5cd272135
Kairat <burambaevkairat@mail.ru>
2022-11-25 16:26:21 +0300

    created array

--- FirstTest.cs
+++ FirstTest.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello GeekBrains!");
+﻿string[] input = new string[6] { "supercontra", "cHo", "0.2f", "4.4", "VanillaIce", "=)" };
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+Print(input);
+

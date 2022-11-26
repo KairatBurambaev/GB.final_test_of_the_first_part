8032811846519e3a503e85bb8ae3094e84b97234
Kairat <burambaevkairat@mail.ru>
2022-11-26 15:58:11 +0300

    added Filtered function

--- FirstTest.cs
+++ FirstTest.cs
@@ -9,4 +9,19 @@ void Print(string[] array)
     Console.WriteLine();
 }
 Print(input);
-
+string[] Filter(string[] input)
+{
+    string[] result = new string[input.Length];
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= 3)
+        {
+            result[count] = input[i];
+            count += 1;
+        }
+    }
+    return result;
+}
+string[] filtered = Filter(input);
+Print(filtered);
